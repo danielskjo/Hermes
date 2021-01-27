8135f84488a429c5eb61651d997e9dcbe6a824c6
Kevin Patel <kevinjaypatel@gmail.com>
2021-01-26 20:12:44 -0800

    added donor screens

---
+++ lib/screens/donor/donor_tabs.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DonorTabs extends StatefulWidget {
+  @override
+  _DonorTabsState createState() => _DonorTabsState();
+}
+
+class _DonorTabsState extends State<DonorTabs> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Donor Tabs'),
+    );
+  }
+}
