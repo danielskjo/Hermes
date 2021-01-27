386bc9fe94f9807eb77c565c11b070ef8c7f0231
Brenden Smith <brendensmith01@gmail.com>
2021-01-27 02:46:43 -0800

    Made page a SingleScrollChildView to prevent overflow with keyboard

--- lib/screens/student/student_register.dart
+++ lib/screens/student/student_register.dart
@@ -18,96 +18,98 @@ class StudentRegisterState extends State<StudentRegister> {
 
     return Scaffold(
       appBar: appBar,
-      body: Column(
-        children: <Widget> [
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget> [
 
-          Padding(
-            padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Name",
-                hintText: "Name",
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Name",
+                  hintText: "Name",
+                ),
               ),
             ),
-          ),
 
-          Padding(
-            padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Username",
-                hintText: "Username",
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Username",
+                  hintText: "Username",
+                ),
               ),
             ),
-          ),
 
-          Padding(
-            padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "School Email",
-                hintText: "School Email",
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "School Email",
+                  hintText: "School Email",
+                ),
               ),
             ),
-          ),
 
-          Padding(
-            padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "University",
-                hintText: "University",
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "University",
+                  hintText: "University",
+                ),
               ),
             ),
-          ),
 
-          Padding(
-            padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Password",
-                hintText: "Password",
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Password",
+                  hintText: "Password",
+                ),
               ),
             ),
-          ),
 
-          Padding(
-            padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Confirm Password",
-                hintText: "Confirm Password",
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Confirm Password",
+                  hintText: "Confirm Password",
+                ),
               ),
             ),
-          ),
-          
-          SizedBox(height: 20),
+            
+            SizedBox(height: 20),
 
-          Container(
-            height: 50,
-            width: 250,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(20),
-            ),
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(20),
+              ),
 
-            child: FlatButton(
-              onPressed: (){
-                // Route to next?
-              },
-              child: Text(
-                "Register",
-                style: TextStyle(color: Colors.white, fontSize: 25),
+              child: FlatButton(
+                onPressed: (){
+                  // Route to next?
+                },
+                child: Text(
+                  "Register",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
