02e54e9132038ffaf81da83a4eefdaec74d448a0
Kevin Patel <kevinjaypatel@gmail.com>
2021-01-26 21:03:37 -0800

    created bottom bar for donor screens

--- lib/screens/donor/donor_tabs.dart
+++ lib/screens/donor/donor_tabs.dart
@@ -1,15 +1,74 @@
 import 'package:flutter/material.dart';
 
+import './donor_home.dart';
+import './donor_chats.dart';
+import './donor_profile.dart';
+
 class DonorTabs extends StatefulWidget {
+
+  static const routeName = '/donor-tabs';
+
   @override
   _DonorTabsState createState() => _DonorTabsState();
 }
 
 class _DonorTabsState extends State<DonorTabs> {
+  List<Map<String, Object>> _pages;
+
+  int _selectedPageIndex = 0;
+
+  @override
+  void initState() {
+    _pages = [
+      {
+        'page': DonorHome(),
+        'title': 'Home',
+      },
+      {
+        'page': DonorChats(),
+        'title': 'Chats',
+      },
+      {
+        'page': DonorProfile(),
+        'title': 'Profile',
+      },
+    ];
+    super.initState();
+  }
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Donor Tabs'),
+    return Scaffold(
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: Theme.of(context).accentColor,
+        unselectedItemColor: Colors.white,
+        currentIndex: _selectedPageIndex,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            title: Text('Chats'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Profile'),
+          ),
+        ],
+      ),
     );
   }
+
 }
