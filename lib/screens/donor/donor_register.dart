03b7e7f27e299d42990e545e6c25ebd356994131
Brenden Smith <brendensmith01@gmail.com>
2021-01-27 02:48:54 -0800

    Created Donor Register page

--- lib/screens/donor/donor_register.dart
+++ lib/screens/donor/donor_register.dart
@@ -9,8 +9,108 @@ class DonorRegister extends StatefulWidget {
 class DonorRegisterState extends State<DonorRegister> {
   @override
   Widget build(BuildContext context) {
+    final AppBar appBar = AppBar(
+      leading: FlutterLogo(),
+      title: Text(
+        'Register as a Donor',
+      ),
+    );
+
     return Scaffold(
-      body: Text("Donor register page"),
+      appBar: appBar,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget> [
+
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Name",
+                  hintText: "Name",
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Username",
+                  hintText: "Username",
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Email",
+                  hintText: "Email",
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Address (Optional)",
+                  hintText: "Address (Optional)",
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Password",
+                  hintText: "Password",
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Confirm Password",
+                  hintText: "Confirm Password",
+                ),
+              ),
+            ),
+            
+            SizedBox(height: 20),
+
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(20),
+              ),
+
+              child: FlatButton(
+                onPressed: (){
+                  // Route to next?
+                },
+                child: Text(
+                  "Register",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
