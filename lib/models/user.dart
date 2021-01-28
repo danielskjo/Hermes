99af340a0a2048e30d032a4ffd0b5916a8397fdd
Brenden Smith <brendensmith01@gmail.com>
2021-01-27 16:40:08 -0800

    Reconfigure user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,37 +1,37 @@
-abstract class User {
+class User {
   String id;
   String name;
   String username;
   String email;
   String password;
+  String image;
+  String type;
   String address;
   String university;
-  String image;
   DateTime date;
-}
 
-class Student extends User {
-  Student(String id, String name, String username, String email, String password, String university, String image, DateTime date) {
+  // Donor constructor
+  DonorUser(String id, String name, String username, String email, String password, String image, String address, DateTime date) {
     this.id = id;
     this.name = name;
     this.username = username;
     this.email = email;
     this.password = password;
-    this.university = university;
     this.image = image;
+    this.type = "Donor";
+    this.address = address;
     this.date = date;
   }
-}
-
-class Donor extends User {
-  Donor(String id, String name, String username, String email, String password, String address, String image, DateTime date) {
+  // Student constructor
+  StudentUser(String id, String name, String username, String email, String password, String image, String university, DateTime date) {
     this.id = id;
     this.name = name;
     this.username = username;
     this.email = email;
     this.password = password;
-    this.university = university;
     this.image = image;
+    this.type = "Student";
+    this.university = university;
     this.date = date;
   }
 }
