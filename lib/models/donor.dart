b47adec7e67c50cf111d17d7e1f7d3d8b43c31c6
Brenden Smith <brendensmith01@gmail.com>
2021-01-27 19:12:53 -0800

    Seperated donor and student

---
+++ lib/models/donor.dart
@@ -0,0 +1,13 @@
+class Donor {
+  String id;
+  String name;
+  String username;
+  String email;
+  String password;
+  String image;
+  String type;
+  String address;
+  DateTime date;
+
+  Donor(String id, String name, String username, String email, String password, String image, String address);
+}
---
+++ lib/models/student.dart
@@ -0,0 +1,13 @@
+class Student {
+  String id;
+  String name;
+  String username;
+  String email;
+  String password;
+  String image;
+  String type;
+  String university;
+  DateTime date;
+
+  Student(String id, String name, String username, String email, String password, String image, String university);
+}
