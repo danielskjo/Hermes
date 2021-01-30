00f8bd8408dfeceaf2a181ba305e5467848c99e7
Daniel Jo <sungkeun.jo@gmail.com>
2021-01-30 13:20:26 -0800

    Remove unnecessary routes

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,9 @@ import './screens/onboarding/student_register.dart';
 
 // Student Screens
 import './screens/student/student_tabs.dart';
-import './screens/student/student_home.dart';
-import './screens/student/student_chats.dart';
-import './screens/student/student_profile.dart';
 
 // Donor Screens
 import './screens/donor/donor_tabs.dart';
-import './screens/donor/donor_home.dart';
-import './screens/donor/donor_chats.dart';
-import './screens/donor/donor_profile.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -57,18 +51,12 @@ class _MyAppState extends State<MyApp> {
           Login.routeName: (ctx) => Login(),
           Register.routeName: (ctx) => Register(),
           StudentTabs.routeName: (ctx) => StudentTabs(),
-          StudentHome.routeName: (ctx) => StudentHome(),
-          StudentChats.routeName: (ctx) => StudentChats(),
-          StudentProfile.routeName: (ctx) => StudentProfile(),
           StudentRegister.routeName: (ctx) => StudentRegister(),
           DonorTabs.routeName: (ctx) => DonorTabs(),
-          DonorHome.routeName: (ctx) => DonorHome(),
-          DonorChats.routeName: (ctx) => DonorChats(),
-          DonorProfile.routeName: (ctx) => DonorProfile(),
           DonorRegister.routeName: (ctx) => DonorRegister(),
         },
         onUnknownRoute: (settings) {
-          return MaterialPageRoute(builder: (ctx) => StudentHome());
+          return MaterialPageRoute(builder: (ctx) => Login());
         },
       ),
     );
@@ -87,36 +75,3 @@ class Wrapper extends StatelessWidget {
     }
   }
 }
-
-// class MainScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return StreamBuilder<User>(
-//       stream: FirebaseAuth.instance.authStateChanges(),
-//       builder: (context, snapshot) {
-//         if(snapshot.hasData && snapshot.data != null) {
-//           UserHelper.saveUser(snapshot.data);
-//           return StreamBuilder<DocumentSnapshot>(
-//             stream: FirebaseFirestore.instance.collection("users").doc(snapshot.data.uid).snapshots() ,
-//             builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot){
-//               if(snapshot.hasData && snapshot.data != null) {
-//                 final userDoc = snapshot.data;
-//                 final user = userDoc.data();
-//                 if(user['role'] == 'admin') {
-//                   return AdminHomePage();
-//                 }else{
-//                   return HomePage();
-//                 }
-//               }else{
-//                 return Material(
-//                   child: Center(child: CircularProgressIndicator(),),
-//                 );
-//               }
-//             },
-//           );
-//         }
-//         return LoginPage();
-//       }
-//     );
-//   }
-// }
