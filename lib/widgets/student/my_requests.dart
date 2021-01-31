3a90e621ddeaefbb2ef5cd2b99d826f9f9128d49
Brenden Smith <brendensmith01@gmail.com>
2021-01-30 20:29:38 -0800

    Converted listview to slidable

--- lib/widgets/student/my_requests.dart
+++ lib/widgets/student/my_requests.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 import '../../models/request.dart';
 
@@ -34,37 +35,91 @@ class _MyRequestsState extends State<MyRequests> {
           )
         : ListView.builder(
             itemBuilder: (ctx, index) {
-              return Card(
-                elevation: 5,
-                margin: EdgeInsets.symmetric(
-                  vertical: 7,
-                  horizontal: 5,
+              return Slidable(
+                actionPane: SlidableDrawerActionPane(),
+                actionExtentRatio: 0.25,
+                child: Container(
+                  padding: const EdgeInsets.only(bottom: 5, top: 5),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        width: 1, 
+                        color: Colors.grey[300],
+                      ),
+                    ),
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.person,
+                    ),
+                    title: Text(
+                      (widget.requests[index].title.length > 20)
+                          ? '${widget.requests[index].title.substring(0, 17)}...'
+                          : '${widget.requests[index].title}',
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                    subtitle: Text(
+                      (widget.requests[index].desc.length > 20)
+                          ? '${widget.requests[index].desc.substring(0, 25)}...\n${DateFormat.yMMMd().format(widget.requests[index].date)}'
+                          : '${widget.requests[index].desc}\n${DateFormat.yMMMd().format(widget.requests[index].date)}',
+                    ),
+                  ),
                 ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.person,
+                secondaryActions: <Widget>[
+                  IconSlideAction(
+                    caption: 'Edit',
+                    color: Colors.grey,
+                    icon: Icons.edit,
+                    onTap: () => widget.editRequest(context, widget.requests[index], index, true),
                   ),
-                  title: Text(
-                    (widget.requests[index].title.length > 20)
-                        ? '${widget.requests[index].title.substring(0, 17)}...'
-                        : '${widget.requests[index].title}',
-                    style: Theme.of(context).textTheme.title,
-                  ),
-                  subtitle: Text(
-                    (widget.requests[index].desc.length > 20)
-                        ? '${widget.requests[index].desc.substring(0, 25)}...\n${DateFormat.yMMMd().format(widget.requests[index].date)}'
-                        : '${widget.requests[index].desc}\n${DateFormat.yMMMd().format(widget.requests[index].date)}',
-                  ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.edit),
-                    onPressed: () {
-                      widget.editRequest(context, widget.requests[index], index, true);
-                    },
+                  IconSlideAction(
+                    caption: 'Delete',
+                    color: Colors.red,
+                    icon: Icons.delete,
+                    onTap: () {},
                   )
-                ),
+                ],
               );
             },
             itemCount: widget.requests.length,
           );
   }
 }
+
+// return Container(
+//                 padding: const EdgeInsets.only(bottom: 15),
+                // decoration: BoxDecoration(
+                //   border: Border(
+                //     bottom: BorderSide(
+                //       width: 1, 
+                //       color: Colors.grey[400],
+                //     ),
+                //   ),
+                // ),
+//                 margin: EdgeInsets.symmetric(
+//                   vertical: 7,
+//                   horizontal: 5,
+//                 ),
+                // child: ListTile(
+                //   leading: Icon(
+                //     Icons.person,
+                //   ),
+                //   title: Text(
+                //     (widget.requests[index].title.length > 20)
+                //         ? '${widget.requests[index].title.substring(0, 17)}...'
+                //         : '${widget.requests[index].title}',
+                //     style: Theme.of(context).textTheme.title,
+                //   ),
+                //   subtitle: Text(
+                //     (widget.requests[index].desc.length > 20)
+                //         ? '${widget.requests[index].desc.substring(0, 25)}...\n${DateFormat.yMMMd().format(widget.requests[index].date)}'
+                //         : '${widget.requests[index].desc}\n${DateFormat.yMMMd().format(widget.requests[index].date)}',
+                //   ),
+                //   trailing: IconButton(
+                //     icon: Icon(Icons.edit),
+                //     onPressed: () {
+                //       widget.editRequest(context, widget.requests[index], index, true);
+                //     },
+//                   )
+//                 ),
+//               );
