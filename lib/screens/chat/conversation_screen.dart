4ffe32457142e9a25384ba83ee1d01fd6b1f0d63
Kevin Patel <kevinjaypatel@gmail.com>
2021-02-10 10:38:47 -0800

    added a chatRoomId property

--- lib/screens/chat/conversation_screen.dart
+++ lib/screens/chat/conversation_screen.dart
@@ -1,11 +1,21 @@
-import 'package:flutter/material.dart'; 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 class ConversationScreen extends StatefulWidget {
+
+  final String chatRoomId;
+
+  ConversationScreen({this.chatRoomId});
+
   @override
   _ConversationScreenState createState() => _ConversationScreenState();
 }
 
 class _ConversationScreenState extends State<ConversationScreen> {
+
+  Stream<QuerySnapshot> chats;
+  TextEditingController messageEditingController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Container(
