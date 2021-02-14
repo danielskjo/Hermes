07d412a3f78c7ec01a36a12f16eff0acb69728f4
Kevin Patel <kevinjaypatel@gmail.com>
2021-02-13 17:50:14 -0800

    created the ui for a conversation among two users

--- lib/screens/chat/conversation_screen.dart
+++ lib/screens/chat/conversation_screen.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:csulb_dsc_2021/services/database.dart';
+import 'package:csulb_dsc_2021/services/helper/constants.dart';
+import 'package:csulb_dsc_2021/widgets/graphics.dart';
 import 'package:flutter/material.dart';
 
 class ConversationScreen extends StatefulWidget {
-
   final String chatRoomId;
 
   ConversationScreen({this.chatRoomId});
@@ -12,14 +14,87 @@ class ConversationScreen extends StatefulWidget {
 }
 
 class _ConversationScreenState extends State<ConversationScreen> {
-
   Stream<QuerySnapshot> chats;
   TextEditingController messageEditingController = new TextEditingController();
 
+  Widget chatMessages() {
+    return StreamBuilder(
+      stream: chats,
+      builder: (context, snapshot) {
+        return snapshot.hasData ? ListView.builder() : Container();
+      },
+    );
+  }
+
+  sendMessage() {
+    if(messageEditingController.text.isNotEmpty) {
+      Map<String, dynamic> chatMessageMap = {
+        "sentBy": Constants.myName,
+        "message": messageEditingController.text,
+        "time": DateTime.now().millisecondsSinceEpoch,
+      };
+
+      DatabaseService().addMessage(widget.chatRoomId, chatMessageMap);
+
+      setState(() {
+        messageEditingController.text = '';
+      });
+    } else {
+      print('Input text is empty');
+    }
+
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Conversation Screen')
+    return Scaffold(
+      appBar: _appBar,
+      body: Container(
+        child: Stack(
+          children: [
+            chatMessages(),
+            Container(
+              alignment: Alignment.bottomCenter,
+              width: MediaQuery.of(context).size.width,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+                color: Colors.grey[300],
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: messageEditingController,
+                        style: TextStyle(color: Colors.black),
+                        decoration: InputDecoration(
+                          hintText: "Message ...",
+                          hintStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: () {
+                        print('Tapped send message');
+                        sendMessage();
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+final _appBar = AppBar(
+  elevation: 0.0,
+);
