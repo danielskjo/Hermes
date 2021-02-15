0f4c1428b0a4470ce107f0c8697219ea761adea2
Kevin Patel <kevinjaypatel@gmail.com>
2021-02-14 21:00:39 -0800

    Conversation screen for current user displays the username of the user they're messaging on the app bar Stream builder is used for displaying new messages

--- lib/screens/chat/conversation_screen.dart
+++ lib/screens/chat/conversation_screen.dart
@@ -1,37 +1,53 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:csulb_dsc_2021/screens/chat/chat_message_tile.dart';
 import 'package:csulb_dsc_2021/services/database.dart';
 import 'package:csulb_dsc_2021/services/helper/constants.dart';
-import 'package:csulb_dsc_2021/widgets/graphics.dart';
 import 'package:flutter/material.dart';
 
 class ConversationScreen extends StatefulWidget {
+  final String chatWithUserName;
   final String chatRoomId;
 
-  ConversationScreen({this.chatRoomId});
+  ConversationScreen({this.chatRoomId, this.chatWithUserName});
 
   @override
   _ConversationScreenState createState() => _ConversationScreenState();
 }
 
 class _ConversationScreenState extends State<ConversationScreen> {
-  Stream<QuerySnapshot> chats;
+  // final String myUserName, myEmail;
+
+  getInfoFromSharedPreference() {}
+
+  /// stream is used for sending and viewing messages in real-time
+  Stream messageStream;
   TextEditingController messageEditingController = new TextEditingController();
 
   Widget chatMessages() {
     return StreamBuilder(
-      stream: chats,
+      stream: messageStream,
       builder: (context, snapshot) {
-        return snapshot.hasData ? ListView.builder() : Container();
+        return snapshot.hasData
+            ? ListView.builder(
+                itemCount: snapshot.data.docs.length,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot ds = snapshot.data.docs[index];
+                  return ChatMessageTile(message: ds['message'], sentByMe: true,);
+                })
+
+            : Center(child: CircularProgressIndicator());
       },
     );
   }
 
   sendMessage() {
-    if(messageEditingController.text.isNotEmpty) {
+    if (messageEditingController.text.isNotEmpty) {
+      DateTime lastMessageTimeStamp = DateTime.now();
+
       Map<String, dynamic> chatMessageMap = {
-        "sentBy": Constants.myName,
         "message": messageEditingController.text,
-        "time": DateTime.now().millisecondsSinceEpoch,
+        "sentBy": Constants.myName,
+        "time-stamp": lastMessageTimeStamp,
       };
 
       DatabaseService().addMessage(widget.chatRoomId, chatMessageMap);
@@ -42,19 +58,38 @@ class _ConversationScreenState extends State<ConversationScreen> {
     } else {
       print('Input text is empty');
     }
+  }
 
+  getAndSetMessages() async {
+    await DatabaseService()
+        .getConversationMessages(widget.chatRoomId)
+        /// after conversation messages are received
+        .then((value) {
+            /// rebuild the ui to reflect the message stream
+            setState(() {
+              messageStream = value;
+            });
+        });
 
+    print('messageStream val: ' + messageStream.toString());
+  }
 
+  @override
+  void initState() {
+    getAndSetMessages();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _appBar,
+      appBar: AppBar(elevation: 0.0, title: Text(widget.chatWithUserName)),
       body: Container(
         child: Stack(
           children: [
+            /// List of Conversation Messages
             chatMessages(),
+            /// Send Message Container
             Container(
               alignment: Alignment.bottomCenter,
               width: MediaQuery.of(context).size.width,
@@ -68,9 +103,9 @@ class _ConversationScreenState extends State<ConversationScreen> {
                         controller: messageEditingController,
                         style: TextStyle(color: Colors.black),
                         decoration: InputDecoration(
-                          hintText: "Message ...",
+                          hintText: "Type a message",
                           hintStyle: TextStyle(
-                            color: Colors.white,
+                            color: Colors.black,
                             fontSize: 16,
                           ),
                         ),
@@ -80,7 +115,6 @@ class _ConversationScreenState extends State<ConversationScreen> {
                     IconButton(
                       icon: Icon(Icons.send),
                       onPressed: () {
-                        print('Tapped send message');
                         sendMessage();
                       },
                     ),
@@ -95,6 +129,45 @@ class _ConversationScreenState extends State<ConversationScreen> {
   }
 }
 
-final _appBar = AppBar(
-  elevation: 0.0,
-);
+class MessageTile extends StatelessWidget {
+  final String message;
+  final bool sendByMe;
+
+  MessageTile({@required this.message, @required this.sendByMe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+          top: 8, bottom: 8, left: sendByMe ? 0 : 24, right: sendByMe ? 24 : 0),
+      alignment: sendByMe ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin:
+            sendByMe ? EdgeInsets.only(left: 30) : EdgeInsets.only(right: 30),
+        padding: EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 20),
+        decoration: BoxDecoration(
+            borderRadius: sendByMe
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(23),
+                    topRight: Radius.circular(23),
+                    bottomLeft: Radius.circular(23))
+                : BorderRadius.only(
+                    topLeft: Radius.circular(23),
+                    topRight: Radius.circular(23),
+                    bottomRight: Radius.circular(23)),
+            gradient: LinearGradient(
+              colors: sendByMe
+                  ? [const Color(0xff007EF4), const Color(0xff2A75BC)]
+                  : [const Color(0x1AFFFFFF), const Color(0x1AFFFFFF)],
+            )),
+        child: Text(message,
+            textAlign: TextAlign.start,
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontFamily: 'OverpassRegular',
+                fontWeight: FontWeight.w300)),
+      ),
+    );
+  }
+}
