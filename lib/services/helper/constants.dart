e918664e3368c38019584fc3978743f494d8a198
Kevin Patel <kevinjaypatel@gmail.com>
2021-02-17 14:06:58 -0800

    created constant data that can be retrieved for the current user such as their username, email, and profile pic url

--- lib/services/helper/constants.dart
+++ lib/services/helper/constants.dart
@@ -1,3 +1,8 @@
+/// The constansts class will
+/// store all constant data relating
+/// to the current user of the application
 class Constants {
-  static String myName = '';
+  static String myUserName = '';
+  static String myEmail = '';
+  static String myProfilePicUrl = '';
 }
