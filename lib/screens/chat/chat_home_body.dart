5de32b69c70264e1169fdc6e5e5edde05fec0f30
Kevin Patel <kevinjaypatel@gmail.com>
2021-02-16 16:00:32 -0800

    added a stateful widget to represent the body of the chat home screen

---
+++ lib/screens/chat/chat_home_body.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ChatHomeBody extends StatefulWidget {
+  @override
+  _ChatHomeBodyState createState() => _ChatHomeBodyState();
+}
+
+class _ChatHomeBodyState extends State<ChatHomeBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
