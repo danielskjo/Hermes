c9ffea598647458b277da2b1a54785edc1704cd0
Kevin Patel <kevinjaypatel@gmail.com>
2021-02-17 13:45:24 -0800

    created a chat home file as the root of all widgets that build the chat view. Ultimately, this widget behaves as a facade and encapsulates the widgets that build the chat view

--- lib/screens/chat/chat_home.dart
+++ lib/screens/chat/chat_home.dart
@@ -1,15 +1,13 @@
-import 'package:csulb_dsc_2021/screens/chat/search_user.dart';
+import 'package:csulb_dsc_2021/screens/chat/chat_home_body.dart';
 import 'package:csulb_dsc_2021/services/helper/constants.dart';
 import 'package:csulb_dsc_2021/services/helper/helperFunctions.dart';
 import 'package:flutter/material.dart';
 
 // Widgets
 import '../../widgets/graphics.dart';
-import '../../models/chat.dart';
-import '../../widgets/chat/chat_constructor.dart';
-
 
 class ChatRoom extends StatefulWidget {
+
   static const routeName = '/chat';
 
   @override
@@ -20,101 +18,29 @@ class _ChatRoomState extends State<ChatRoom> {
 
   @override
   void initState() {
-    getUserInfo();
+    setUserInfo();
   }
 
-  getUserInfo() async {
-    Constants.myName = await HelperFunctions.getUserNameSharedPreference();
-  }
-  final List<Chat> messages = [
-    Chat(DateTime.now().toString(), 'Lydia Yang', 'I want chicken.',
-        DateTime.now()),
-    Chat(DateTime.now().toString(), 'Brenden Smith',
-        'Playing Destiny 2 right now!', DateTime.now()),
-    Chat(DateTime.now().toString(), 'Daniel Jo',
-        'I am writing Firebase right now', DateTime.now()),
-    Chat(DateTime.now().toString(), 'Kevin Jay Patel',
-        'Come to the DSC zoom meeting!', DateTime.now()),
-    Chat(DateTime.now().toString(), 'Grim Reaper', 'Knock knock',
-        DateTime.now()),
-    Chat(DateTime.now().toString(), 'Eren Jaegar',
-        'I\'m gonna join the Scout Regiment!', DateTime.now()),
-    Chat(DateTime.now().toString(), 'Armin Arlet',
-        'I\'m very sad I am getting beat up', DateTime.now()),
-    Chat(DateTime.now().toString(), 'Mikasa Ackerman', 'Where is Eren',
-        DateTime.now()),
-  ];
-
-  void _deleteConversation(String id) {
-    setState(() {
-      messages.removeWhere((request) {
-        return request.id == id;
-      });
-    });
+  /// Initalizes the current users username and email
+  /// so that it can be used throughout the application
+  setUserInfo() async {
+    Constants.myUserName = await HelperFunctions().getUserName();
+    Constants.myEmail = await HelperFunctions().getUserEmail();
   }
 
   @override
   Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
 
     final AppBar appBar = AppBar(
       leading: SmallLogo(50),
       elevation: 0.0,
       title: Text('My Messages'),
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.search),
-          onPressed: () => showSearch(
-              context: context,
-              delegate: SearchUsers()),
-        ),
-      ],
-    );
-
-
-    final Container messageList = Container(
-      height: (mediaQuery.size.height -
-              appBar.preferredSize.height -
-              mediaQuery.padding.top) *
-          1,
-      padding: const EdgeInsets.only(bottom: 50),
-      child: Material(child: ChatWidget(messages, _deleteConversation, false)),
-    );
-
-    final pageBody = Container(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(30),
-            topRight: Radius.circular(30),
-          ),
-        ),
-        height: (mediaQuery.size.height -
-                appBar.preferredSize.height -
-                mediaQuery.padding.top) *
-            1,
-        child: ClipRRect(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-          child: Column(
-            children: <Widget> [
-              Expanded(
-                flex: 2,
-                child: SingleChildScrollView(
-                  child: messageList,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      backgroundColor: Theme.of(context).primaryColor,
     );
 
     return Scaffold(
       appBar: appBar,
-      body: pageBody,
-      backgroundColor: Theme.of(context).primaryColor,
+      body: ChatHomeBody(),
     );
   }
 }
