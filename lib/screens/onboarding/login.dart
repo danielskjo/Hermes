e9c221b113643d99f8120cc7cade5e6fe6ea73b6
Daniel Jo <sungkeun.jo@gmail.com>
2021-03-01 17:01:01 -0800

    Split up login screen widgets

--- lib/screens/onboarding/login.dart
+++ lib/screens/onboarding/login.dart
@@ -36,149 +36,150 @@ class LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
+    final registerText = Container(
+      height: 40,
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Text("Don't have an account?"),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(Register.routeName);
+              },
+              child: Text(
+                "Register",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    final emailFormField = TextFormField(
+      controller: _emailController,
+      validator: (val) => val.isEmpty ? 'Enter your email' : null,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: "Email",
+        hintText: "Email",
+        fillColor: Colors.white,
+        filled: true,
+      ),
+      onFieldSubmitted: (_) {
+        FocusScope.of(context).requestFocus(_passwordFocusNode);
+      },
+    );
+
+    final passwordFormField = TextFormField(
+      controller: _passwordController,
+      validator: (val) => val.isEmpty ? 'Enter your password' : null,
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: "Password",
+        hintText: "Password",
+        fillColor: Colors.white,
+      ),
+      focusNode: _passwordFocusNode,
+      onFieldSubmitted: (_) {
+        submitAction();
+      },
+    );
+
+    final pageBody = SingleChildScrollView(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 100),
+              Logo(),
+              Container(
+                padding: const EdgeInsets.only(
+                  left: 15,
+                  right: 15,
+                ),
+                height: 100,
+                child: Center(
+                  child: Text(
+                    "Hermes",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 26,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(
+                  left: 15,
+                  right: 15,
+                  bottom: 15,
+                ),
+                height: 90,
+                child: emailFormField,
+              ),
+              Container(
+                padding: const EdgeInsets.only(
+                  left: 15,
+                  right: 15,
+                ),
+                height: 90,
+                child: passwordFormField,
+              ),
+              Container(
+                height: 50,
+                child: Text(
+                  error,
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 14.0,
+                  ),
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 250,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(
+                    20,
+                  ),
+                ),
+                child: FlatButton(
+                  onPressed: () {
+                    submitAction();
+                  },
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+              ),
+              Spacer(),
+              registerText,
+              SizedBox(height: 30),
+            ],
+          ),
+        ),
+      ),
+    );
+
     return loading
         ? Loading()
         : Scaffold(
             backgroundColor: Colors.white,
-            body: SingleChildScrollView(
-              child: Container(
-                height: MediaQuery.of(context).size.height,
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 100),
-                      Logo(),
-                      Container(
-                        padding: const EdgeInsets.only(
-                          left: 15,
-                          right: 15,
-                        ),
-                        height: 100,
-                        child: Center(
-                          child: Text(
-                            "Hermes",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 26,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.only(
-                          left: 15,
-                          right: 15,
-                          top: 0,
-                          bottom: 15,
-                        ),
-                        height: 90,
-                        child: TextFormField(
-                          controller: _emailController,
-                          validator: (val) =>
-                              val.isEmpty ? 'Enter your email' : null,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: "Email",
-                            hintText: "Email",
-                            fillColor: Colors.white,
-                            filled: true,
-                          ),
-                          onFieldSubmitted: (_) {
-                            FocusScope.of(context)
-                                .requestFocus(_passwordFocusNode);
-                          },
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.only(
-                          left: 15,
-                          right: 15,
-                          top: 0,
-                          bottom: 0,
-                        ),
-                        height: 90,
-                        child: TextFormField(
-                          controller: _passwordController,
-                          validator: (val) =>
-                              val.isEmpty ? 'Enter your password' : null,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: "Password",
-                            hintText: "Password",
-                            fillColor: Colors.white,
-                          ),
-                          focusNode: _passwordFocusNode,
-                          onFieldSubmitted: (_) {
-                            submitAction();
-                          },
-                        ),
-                      ),
-                      Container(
-                        height: 50,
-                        child: Text(
-                          error,
-                          style: TextStyle(
-                            color: Colors.red,
-                            fontSize: 14.0,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 50,
-                        width: 250,
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.circular(
-                            20,
-                          ),
-                        ),
-                        child: FlatButton(
-                          onPressed: () {
-                            submitAction();
-                          },
-                          child: Text(
-                            "Login",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 25,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Spacer(),
-                      Container(
-                        height: 40,
-                        child: Column(
-                          children: <Widget>[
-                            Expanded(
-                              child: Text("Don't have an account?"),
-                            ),
-                            Expanded(
-                              child: FlatButton(
-                                onPressed: () {
-                                  Navigator.of(context)
-                                      .pushNamed(Register.routeName);
-                                },
-                                child: Text(
-                                  "Register",
-                                  style: TextStyle(
-                                    color: Colors.blue,
-                                    fontSize: 15,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: 30),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            body: pageBody,
           );
   }
 
