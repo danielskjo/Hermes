65d278936123d7ac2f7437d2a53287065f9bfc6f
Kevin Patel <kevinjaypatel@gmail.com>
2021-03-08 17:21:48 -0800

    update

--- lib/screens/chat/home_body.dart
+++ lib/screens/chat/home_body.dart
@@ -12,7 +12,6 @@ import 'conversation_tile.dart';
 /// for the current user to communicate with
 
 class ChatHomeBody extends StatefulWidget {
-
   double appBarHeight;
 
   ChatHomeBody({this.appBarHeight});
@@ -22,7 +21,6 @@ class ChatHomeBody extends StatefulWidget {
 }
 
 class _ChatHomeBodyState extends State<ChatHomeBody> {
-
   bool isSearching = false;
   TextEditingController searchUserName = TextEditingController();
   Stream usersFound, existingConversations;
@@ -41,11 +39,10 @@ class _ChatHomeBodyState extends State<ChatHomeBody> {
   }
 
   getExistingConversations() async {
-    await DatabaseService().getChatRooms()
-        .then((value) {
-          setState(() {
-            existingConversations = value;
-          });
+    await DatabaseService().getChatRooms().then((value) {
+      setState(() {
+        existingConversations = value;
+      });
     });
   }
 
@@ -53,7 +50,7 @@ class _ChatHomeBodyState extends State<ChatHomeBody> {
     return StreamBuilder(
       stream: usersFound,
       builder: (context, snapshot) {
-        if(snapshot.hasData) {
+        if (snapshot.hasData) {
           return ListView.builder(
             itemCount: snapshot.data.docs.length,
             shrinkWrap: true,
@@ -65,11 +62,13 @@ class _ChatHomeBodyState extends State<ChatHomeBody> {
               );
             },
           );
-        } else if(snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator(),);
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
         } else {
-          print('Error trying to display searched users in the chat home body');
-          return Text('Search results could not be found');
+            print('Error trying to display searched users in the chat home body');
+            return Text('Search results could not be found');
         }
       },
     );
@@ -77,31 +76,31 @@ class _ChatHomeBodyState extends State<ChatHomeBody> {
 
   Widget existingConversationsList() {
     return StreamBuilder(
-      stream: existingConversations,
-      builder: (context, snapshot) {
-        if(snapshot.hasData) {
-          return ListView.builder(
-            itemCount: snapshot.data.docs.length,
-            shrinkWrap: true,
-            itemBuilder: (context, index) {
-              DocumentSnapshot documentSnapshot = snapshot.data.docs[index];
-              print('last message: ' + documentSnapshot['lastMessage']);
-              print("document snapshot id: " + documentSnapshot.id);
-              return ConversationTile(documentSnapshot: documentSnapshot);
-            },
-          );
-        } else if(snapshot.connectionState == ConnectionState.waiting) {
+        stream: existingConversations,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data.docs.length,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                DocumentSnapshot documentSnapshot = snapshot.data.docs[index];
+                print('last message: ' + documentSnapshot['lastMessage']);
+                print("document snapshot id: " + documentSnapshot.id);
+                return ConversationTile(documentSnapshot: documentSnapshot);
+              },
+            );
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
-        } else {
-            return Container(child: Text('No existing messages'),);
-        }
-      }
-    );
+          } else {
+            return Container(
+              child: Text('No existing messages'),
+            );
+          }
+        });
   }
 
   @override
   Widget build(BuildContext context) {
-
     final mediaQuery = MediaQuery.of(context);
 
     return SingleChildScrollView(
@@ -126,26 +125,32 @@ class _ChatHomeBodyState extends State<ChatHomeBody> {
             children: [
               /// Search User Bar
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
+                padding: const EdgeInsets.symmetric(horizontal: 10),
                 child: Row(
                   children: [
                     /// Only display arrow back icon when user is searching
-                    isSearching ? GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          isSearching = false;
-                          searchUserName.text = "";
-                        });
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 12.0,),
-                        child: Icon(Icons.arrow_back),
-                      ),
-                    ) : Container(),
+                    isSearching
+                        ? GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                isSearching = false;
+                                searchUserName.text = "";
+                              });
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                right: 12.0,
+                              ),
+                              child: Icon(Icons.arrow_back),
+                            ),
+                          )
+                        : Container(),
                     /// Textfield for searching for a user
                     Expanded(
                       child: Container(
-                        margin: EdgeInsets.symmetric(vertical: 16,),
+                        margin: EdgeInsets.symmetric(
+                          vertical: 16,
+                        ),
                         padding: EdgeInsets.symmetric(horizontal: 16),
                         decoration: BoxDecoration(
                           border: Border.all(
@@ -170,11 +175,11 @@ class _ChatHomeBodyState extends State<ChatHomeBody> {
                             /// Gesture Detection logic when search icon is tapped
                             IconButton(
                               onPressed: () async {
-                                if(searchUserName.text.isNotEmpty) {
-                                    onSearchButtonClicked();
+                                if (searchUserName.text.isNotEmpty) {
+                                  onSearchButtonClicked();
                                 } else {
-                                    /// TODO: Display snackbar notifying user to input text to search for a user
-                                    print('Textfield is empty');
+                                  /// TODO: Display snackbar notifying user to input text to search for a user
+                                  print('Textfield is empty');
                                 }
                               },
                               icon: Icon(Icons.search),
